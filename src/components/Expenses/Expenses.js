a6a44fb1854fd8191dca839966ffd7adfe540d38
Cameron <cameroningratta@hotmail.com>
2023-06-12 16:26:18 -0400

    Lecture 62

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,63 +1,29 @@
 import ExpenseItem from "./ExpenseItem";
-import Card from '../UI/Card'
-import './Expenses.css'
+import Card from "../UI/Card";
+import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 function Expenses(props) {
+  const saveExpenseYearHandler = (selectedYear) => {
+    setYearSelected(selectedYear);
+  };
 
-    const saveExpenseYearHandler = (selectedYear) => {
+  const [yearSelected, setYearSelected] = useState("2020");
 
-        setYearSelected(selectedYear);
-        console.log(yearSelected);
-   
- }
-
-    const [yearSelected, setYearSelected] =  useState('2020');
-    const expenses = props.expenses;
-
-    
- 
-
-
-
-
-
-    return(
-        
-            
-       
-       <Card className="expenses">
-       <ExpensesFilter selected = {yearSelected} onSaveExpenseYear = {saveExpenseYearHandler} />
-       <ExpenseItem
-       title = {expenses[0].title}
-       amount = {expenses[0].amount}
-       date = {expenses[0].date}
-        />
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={yearSelected}
+        onSaveExpenseYear={saveExpenseYearHandler}
+      />
+      {props.expenses.map((expense) => (
         <ExpenseItem
-       title = {expenses[1].title}
-       amount = {expenses[1].amount}
-       date = {expenses[1].date}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
         />
-        <ExpenseItem
-       title = {expenses[2].title}
-       amount = {expenses[2].amount}
-       date = {expenses[2].date}
-        />
-        <ExpenseItem
-       title = {expenses[3].title}
-       amount = {expenses[3].amount}
-       date = {expenses[3].date}
-        />
-       </Card>
-    
-
-
-
-    )
-
-
-    
-
-
+      ))}
+    </Card>
+  );
 }
 export default Expenses;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,31 +1,23 @@
 import React from "react";
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 const NewExpense = (props) => {
-    
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData ={
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        };
-        console.log(expenseData);
-        props.onAddExpense();
-    }
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+    props.onAddExpense();
+  };
 
-
-return(
+  return (
     <div>
-   
-        <div className="new-expense">
-            <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler}/>
-        </div>
-    
-
-
+      <div className="new-expense">
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      </div>
     </div>
-
-)
-
+  );
 };
 
 export default NewExpense;
