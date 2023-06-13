00d0538c73a71d697634434d40b0a0c5eff8d6d8
Cameron Ingratta <camingratta@gmail.com>
2023-06-13 19:21:26 -0400

    Finished Filter... Added logic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,21 +9,27 @@ function Expenses(props) {
   };
 
   const [yearSelected, setYearSelected] = useState("2020");
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullyear().toString() === selectedYear;
+  });
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={yearSelected}
-        onSaveExpenseYear={saveExpenseYearHandler}
-      />
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={yearSelected}
+          onSaveExpenseYear={saveExpenseYearHandler}
         />
-      ))}
-    </Card>
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key ={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
-}
+};
 export default Expenses;
